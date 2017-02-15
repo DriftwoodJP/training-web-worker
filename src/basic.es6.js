8efa8846adf561d019f5f783d484724bdc7f51e5
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-02-15 16:50:57 +0900

    Add: Web Worker sample

--- src/basic.es6.js
+++ src/basic.es6.js
@@ -1,3 +1,23 @@
 'use strict';
 
 require('babel-polyfill');
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn').addEventListener('click', function() {
+    let worker = new Worker('lib/worker.js');
+
+    worker.postMessage({
+      'target': document.getElementById('target').value,
+      'x': document.getElementById('x').value,
+    });
+    document.getElementById('result').textContent = '計算中...';
+
+    worker.addEventListener('message', function(event) {
+      document.getElementById('result').textContent = event.data;
+    }, false);
+
+    worker.addEventListener('error', function(event) {
+      document.getElementById('result').textContent = event.message;
+    }, false);
+  }, false);
+}, false);
