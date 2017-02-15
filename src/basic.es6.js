5726fc1481443c2ee307e2166eece94b0dc3658b
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-02-15 14:44:59 +0900

    Initial commit

--- src/basic.es6.js
+++ src/basic.es6.js
@@ -1,13 +1,3 @@
 'use strict';
 
 require('babel-polyfill');
-
-let str = 'console';
-if (str) {
-  let str = 'browser';
-  document.write(`Hello, ${str}!`);
-}
-console.log(`Hello, ${str}!`);
-
-import * as math from './lib/math.es6.js';
-console.log('2π = ' + math.sum(math.pi, math.pi));
